06e1a980cf0e76e4a716e62cb2ef799fd396b895
Mr.tao <staugur@saintic.com>
2016-12-21 01:36:53 +0800

    swarm

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -9,8 +9,10 @@ ui_blueprint = Blueprint("ui", __name__, template_folder="templates", static_fol
 @ui_blueprint.route("/")
 def index():
     if g.auth:
-         Swarms      = g.swarm.GET("all", True)
-         ActiveSwarm = g.swarm.GET("active")
+         Swarms      = g.swarm.GET("all", True).get("data")
+         ActiveSwarm = g.swarm.GET("active").get("data")
+         logger.debug(Swarms)
+         logger.debug(ActiveSwarm)
          return render_template("index.html", Swarms=Swarms, SwarmsLength=len(Swarms), ActiveSwarm=ActiveSwarm)
     else:
         abort(403)
