e7ed2a57796d79c16c606ab45b2df93ec1aeb501
Mr.tao <staugur@saintic.com>
2016-12-23 00:24:28 +0800

    auth

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def before_request():
     g.sessionId = request.cookies.get("sessionId", "")
     g.username  = request.cookies.get("username", "")
     g.expires   = request.cookies.get("time", "")
-    g.auth      = True #isLogged_in('.'.join([ g.username, g.expires, g.sessionId ]))
+    g.auth      = isLogged_in('.'.join([ g.username, g.expires, g.sessionId ]))
     g.swarm     = swarm
     g.service   = ServiceManager(ActiveSwarm=g.swarm.getAcitve)
     g.node      = NodeManager(ActiveSwarm=g.swarm.getAcitve)
