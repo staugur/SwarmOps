a99b59fa4d58fd4ea4b7da1ec7803224b9efade0
Mr.tao <staugur@vip.qq.com>
2017-01-17 18:56:48 +0800

    UI调整中

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -7,25 +7,56 @@ from flask import Blueprint, render_template, url_for, redirect, g, request
 
 ui_blueprint = Blueprint("ui", __name__, template_folder="templates", static_folder='static')
 
+''' swarm route'''
 @ui_blueprint.route("/")
 @ui_blueprint.route("/swarm/")
 def index():
     if g.auth:
-        return render_template("swarm.html")
+        return render_template("swarm/swarm.html")
     else:
         return redirect(url_for("login"))
 
+'''service route'''
 @ui_blueprint.route("/service/")
 def service():
     if g.auth:
-        return render_template("service.html")
+        return render_template("service/service.html")
     else:
         return redirect(url_for("login"))
 
+@ui_blueprint.route("/service/delete/")
+def service_delete():
+    if g.auth:
+        return render_template("service/delete.html")
+    else:
+        return redirect(url_for("login"))
+
+@ui_blueprint.route("/service/update/")
+def service_update():
+    if g.auth:
+        return render_template("service/update.html")
+    else:
+        return redirect(url_for("login"))
+
+@ui_blueprint.route("/service/create/")
+def service_create():
+    if g.auth:
+        return render_template("service/create.html")
+    else:
+        return redirect(url_for("login"))
+
+@ui_blueprint.route("/service/detail/")
+def service_detail():
+    if g.auth:
+        return render_template("service/detail.html")
+    else:
+        return redirect(url_for("login"))
+
+'''node route'''
 @ui_blueprint.route("/node/")
 def node():
     if g.auth:
-        return render_template("node.html")
+        return render_template("node/node.html")
     else:
         return redirect(url_for("login"))
 
@@ -39,21 +70,21 @@ def misc():
 @ui_blueprint.route("/storage/")
 def storage():
     if g.auth:
-        return render_template("storage.html")
+        return render_template("misc/storage.html")
     else:
         return redirect(url_for("login"))
 
 @ui_blueprint.route("/nginx/")
 def nginx():
     if g.auth:
-        return render_template("nginx.html")
+        return render_template("service/nginx.html")
     else:
         return redirect(url_for("login"))
 
 @ui_blueprint.route("/network/")
 def network():
     if g.auth:
-        return render_template("network.html")
+        return render_template("network/network.html")
     else:
         return redirect(url_for("login"))
 
