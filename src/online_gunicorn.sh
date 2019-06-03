4219ccbb24b79f87ec8806b05244f29565955102
Mr.tao <staugur@vip.qq.com>
2019-06-03 16:33:18 +0800

    Update online_gunicorn.sh

--- src/online_gunicorn.sh
+++ src/online_gunicorn.sh
@@ -17,7 +17,7 @@ fi
 host=$(python -c "from config import GLOBAL;print GLOBAL['Host']")
 port=$(python -c "from config import GLOBAL;print GLOBAL['Port']")
 procname=$(python -c "from config import GLOBAL;print GLOBAL['ProcessName']")
-cpu_count=$[$(cat /proc/cpuinfo | grep "processor" | wc -l)*2]
+cpu_count=$(cat /proc/cpuinfo | grep "processor" | wc -l)
 [ -d ${dir}/logs ] || mkdir -p ${dir}/logs
 logfile=${dir}/logs/gunicorn.log
 pidfile=${dir}/logs/${procname}.pid
